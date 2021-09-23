56310d9756d3ffc2683a82eebf8c111f046f701a
Gio Bugtong <nlbugtong@gmail.com>
2021-09-23 20:11:52 +0800

    Include Contact link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ export default function Footer() {
         <footer className="py-3">
             <Row className="mx-auto text-center justify-content-center align-items-center" style={{maxWidth: "1000px"}}>
                 <Col className="" xs={12} md={4} ><a href="#home">Home</a></Col>
-                <Col className="" xs={12} md={4} ><a href="#link">Link</a></Col>
                 <Col className="" xs={12} md={4} ><a href="#about">About</a></Col>
+                <Col className="" xs={12} md={4} ><a href="#contact">Contact</a></Col>
             </Row>
         </footer>
         </>
